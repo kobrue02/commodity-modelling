7a63ee2de5ddd16b7826a70f3ecbd54c34c4d0e2
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2024-02-11 23:25:21 +0100

    added two types of LSTMs and a data processing pipeline

--- models/models.py
+++ models/models.py
@@ -1,7 +1,20 @@
-import xgboost as xgb
+import numpy as np
 import polars as pl
+import xgboost as xgb
 
-class Model:
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+from keras.wrappers.scikit_learn import KerasRegressor
+
+import tensorflow as tf
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+import keras
+from keras import layers
+
+class XGBModel:
     def __init__(self,
                  train_set: pl.DataFrame,
                  test_set: pl.DataFrame,
@@ -18,30 +31,128 @@ class Model:
     def train(self):
 
         X_train = self.train_set.select(self.features).to_numpy()
-        y_train = self.train_set.select(self.target).to_numpy()
-
         X_test = self.test_set.select(self.features).to_numpy()
-        y_test = self.test_set.select(self.target).to_numpy()
 
+        if isinstance(self.model, xgb.XGBRegressor):
+            y_test = self.test_set.select(self.target).to_numpy()
+            y_train = self.train_set.select(self.target).to_numpy()
+
+        elif isinstance(self.model, xgb.XGBClassifier):
+            labels = {
+                'buy': 0,
+                'hold': 1,
+                'sell': 2
+            }
+
+            y_train = self.train_set.get_column(self.target[0]).to_list()
+            y_train = [labels[item] for item in y_train]
+            y_test = self.test_set.get_column(self.target[0]).to_list()
+            y_test = [labels[item] for item in y_test]
+            
         self.model.fit(
             X_train, y_train,
             eval_set=[(X_train, y_train), (X_test, y_test)],
             verbose=100
             )
         
+        return {0: 'buy', 1: 'hold', 2: 'sell'}
+        
     def predict(self):
         return self.model.predict(self.test_set[self.features])
+    
+    def feature_importances(self):
+        data = {k: v for k, v in zip(self.features, self.model.feature_importances_)}
+        return data
 
-class XGBRegressor(Model):
 
-    def __init__(self, **kwargs) -> None:
+class XGBRegressor(XGBModel):
+
+    def __init__(self, params: dict, **kwargs) -> None:
         super().__init__(**kwargs)
         
-        self.model: xgb.XGBRegressor = xgb.XGBRegressor(
-            booster='gbtree',
-            n_estimators=500,
-            early_stopping_rounds=100,
-            objective='reg:squarederror',
-            max_depth=12,
-            learning_rate=0.01
-        )
+        self.model: xgb.XGBRegressor = xgb.XGBRegressor(**params)
+
+class XGBClassifier(XGBModel):
+
+    def __init__(self, params: dict, **kwargs) -> None:
+        super().__init__(**kwargs)
+        
+        self.model: xgb.XGBClassifier = xgb.XGBClassifier(**params)
+
+class LSTM:
+
+    def __init__(self) -> None:
+        self.model: keras.Sequential
+
+    def train(self, X_train, y_train, X_test, y_test):
+
+        history = self.model.fit(
+            X_train,
+            y_train,
+            batch_size=64,
+            epochs=100,
+            validation_data=(X_test, y_test)
+            )
+        
+class CategoricalLSTM(LSTM):
+
+    def __init__(
+            self,
+            dim: tuple[int],
+            n_classes: int,
+            params: dict = {},
+            **kwargs
+            ) -> None:
+        
+        super().__init__()
+        self.model = self.initialize_model(dim, n_classes)
+
+    def initialize_model(self,  dim: tuple[int], n_classes: int):
+
+        model = keras.Sequential()
+        model.add(layers.LSTM(64, input_shape=dim))
+        model.add(layers.BatchNormalization())
+        model.add(layers.Dense(n_classes, activation='softmax'))
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+            )
+        return model
+    
+    def predict(self, test_set):
+        return np.argmax(self.model.predict(test_set), axis=1)
+    
+
+class RegressiveLSTM(LSTM):
+    def __init__(
+            self,
+            dim: tuple[int],
+            params: dict = {},
+            **kwargs
+            ) -> None:
+        
+        super().__init__()
+        self.model = self.initialize_model(dim)
+    
+    def initialize_model(self,  dim: tuple[int]):
+        # Build the LSTM model
+        model = keras.Sequential()
+        model.add(layers.LSTM(50, return_sequences=True, input_shape=dim))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.LSTM(50, return_sequences=True))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.LSTM(50))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(32, activation='relu'))
+        model.add(layers.Dense(1))
+        model.compile(optimizer='adam', loss='mean_squared_error')
+        return model
+        
+    def predict(self, test_set):
+        return self.model.predict(test_set)
+    
+
+    
--- models/prepare_data.py
+++ models/prepare_data.py
@@ -1,3 +1,69 @@
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
 
-tss = TimeSeriesSplit(n_splits=5, test_size=12, gap=1)
+import polars as pl
+import numpy as np
+
+class DataSet:
+    def __init__(self, data: pl.DataFrame, target_column: str, date_column: str = None, categorical: bool = False, scale: bool = False) -> None:
+        self.scaler = MinMaxScaler()
+        self.encoder = LabelEncoder()
+        self.poly = PolynomialFeatures(degree=2)
+        self.date_column = date_column
+        self.target_column = target_column
+        self._categorical = categorical
+        self._scale = scale
+        self.X, self.y = self.preprocess_data(data)
+
+
+    def preprocess_data(self, data: pl.DataFrame):
+
+        if self.date_column:
+            x_data = data.drop([self.date_column, self.target_column])
+        else:
+            x_data = data.drop([self.target_column])  
+
+        y_data = data.get_column(self.target_column)
+
+        if self._categorical:
+            # normalize the data
+            X = self.scaler.fit_transform(x_data)
+            # encode target variable
+            y = self.encoder.fit_transform(y_data)
+            y = to_categorical(y)  # convert to one-hot encoded format
+        
+        else:
+            if self._scale:
+                X = self.scaler.fit_transform(x_data.to_numpy())
+                y_reshaped = y_data.to_numpy().reshape(-1, 1)
+                y = self.scaler.fit_transform(y_reshaped).flatten()
+            else:
+                X = x_data.to_numpy()
+                y = y_data.to_numpy()
+
+
+        X = self.poly.fit_transform(X)
+        # reshape data for LSTM input [samples, time steps, features]
+        X_reshaped = X.reshape(X.shape[0], 1, X.shape[1])
+
+        return X_reshaped, y
+
+    def split_data(self, test_size=0.2) -> tuple:
+        # split data into training and testing sets
+        cutoff = int(len(self.X) * (1 - test_size))
+        X_train = self.X[:cutoff]
+        X_test = self.X[cutoff:]
+        y_train = self.y[:cutoff]
+        y_test = self.y[cutoff:]
+        return X_train, X_test, y_train, y_test
+    
+    def decode(self, valueArray):
+        try:
+            return self.encoder.inverse_transform(valueArray)
+        except ValueError:
+            return self.encoder.inverse_transform(np.argmax(valueArray, axis=1))
+    
+    def unscale(self, valueArray):
+        predictions = valueArray.reshape(-1, 1)
+        return self.scaler.inverse_transform(predictions).flatten()
